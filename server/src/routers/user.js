27a8964e823935348f9fbb2b2c38126ae957604c
Blake Benfield <Blakefbenfield101@gmail.com>
2025-11-06 20:48:50 -0800

    Implemented basic user routing

---
+++ server/src/routers/user.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db')
+const {checkAuth} = require("./auth");
+const FormData = require('form-data');
+const Mailgun = require('mailgun.js')
+
+router.delete('/user', checkAuth, async (req, res) => {
+    await db.query('DELETE FROM users WHERE id = ?', [req.user.id]); //TODO can fail
+    req.status(200);
+});
+
+router.post('/user/forgot-password', async (req, res) => {
+    const mailgun = new Mailgun(FormData);
+    const mg = mailgun.client({
+        username: 'api',
+        key: process.env.MAILGUN_API_KEY
+    });
+    try {
+        const data = await mg.messages.create("userservices.peterployment.com", {
+            from: "<server@userservices.peterployment.com>",
+            to: [req.body.email], //TODO check if email valid and exists in db, also create a password reset table.
+            subject: "Password Reset - Peterployment",
+            text: "Yo, reset your password here: https://peterployment.com/user/forgot-password?token=abc123"
+        });
+    } catch (e) {
+        console.log(e);
+    }
+    res.status(200).send("Success!");
+});
+
+module.exports = router;

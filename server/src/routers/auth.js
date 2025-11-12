13b43a2b829c5de54d6b7d5b2f06431230ff4132
Blake Benfield <Blakefbenfield101@gmail.com>
2025-11-12 11:00:30 -0800

    Added better error messages, fixed hashing

--- server/src/routers/auth.js
+++ server/src/routers/auth.js
@@ -1,21 +1,25 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const db = require('../db');
+const argon2 = require('argon2');
 
 // From passport docs
 passport.use(new LocalStrategy(async (email, password, cb) => {
     try {
         const [results, fields] = await db.query("SELECT * FROM users WHERE email=?", [email]);
 
-        let hash = "1"
+        if (results.length < 1) {
+            await argon2.hash("a")
+            return cb(null, false, { message: "Incorrect email or password." });
+        }
 
-        if (results.length > 0) hash = results[0].password;
+        const hash = results[0].password;
         let isValid = await argon2.verify(hash, password);
 
         if (isValid) {
             return cb(null, results[0]);
         } else {
-            return cb(null, false, { message: "Incorrect email or password." })
+            return cb(null, false, { message: "Incorrect email or password." });
         }
     } catch (e) {
         return cb(e);
--- server/src/routers/login.js
+++ server/src/routers/login.js
@@ -7,10 +7,9 @@ const validator = require('../schemas/validator');
 const passport = require('passport');
 const { checkAuth } = require("./auth");
 
-router.post('/login/password', passport.authenticate('local', {
-    successRedirect: '/app',
-    failureRedirect: '/login/?error=invalid'
-}));
+router.post('/login/password', passport.authenticate('local', { successRedirect: '/app' }), (req, res) => {
+    res.status(400).send();
+});
 
 router.post('/logout', checkAuth, (req, res, next) =>{
     req.logout((err)=>{
@@ -20,13 +19,14 @@ router.post('/logout', checkAuth, (req, res, next) =>{
 });
 
 router.post('/signup', async (req, res, next) => {
-    if (!validator(req.body, 'user')) return res.status(400).send();
+    if (!validator(req.body, 'user')) return res.status(400).json({ message: "Bad request" });
     const password_hash = await argon2.hash(req.body.password);
     try {
-        db.query("INSERT INTO users (email, password) VALUES (?, ?)", [req.body.email, password_hash]); //TODO may return failure
-        res.status(200).redirect('/login');
+        await db.query("INSERT INTO users (email, password) VALUES (?, ?)", [req.body.email, password_hash]); //TODO may return failure
+        res.status(200).json({ message: "User created successfully" });
     } catch (e) {
-        res.status(400).send("Bad request") //TODO make error page
+        if (e.code === "ER_DUP_ENTRY") return res.status(400).json({ message: "User already exists!" });
+        res.status(400).json({ message: "Bad request" });
     }
 });
 

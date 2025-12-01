b50bcd9f87760ae46b640326d7cdc7b32ccf3c2c
Blake Benfield <Blakefbenfield101@gmail.com>
2025-11-30 17:47:38 -0800

    Added tabs to multi-table

--- client/src/features/ppapp/components/multi-table.jsx
+++ client/src/features/ppapp/components/multi-table.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import Table from "./table.jsx";
+import TabSVG from '../../../assets/tab.svg?react'
 
 const MultiTable = () => {
     const [tables, setTables] = useState([]);
@@ -15,8 +16,47 @@ const MultiTable = () => {
         init();
     }, []);
 
+    const handleTabSelect = (id) => {
+        setTableSelection(id);
+    }
+
+    const Tab = ({className, tableId, includeOverlap, zIndex, text}) => {
+        let fittedText = text;
+        if (text.length > 8) fittedText = text.substring(0, 5) + '...';
+        if (tableSelection === tableId) zIndex += 50;
+        return (
+            <div className={`relative w-[var(--tabWidth)] cursor-pointer ${includeOverlap ? "-ml-[calc(var(--tabWidth)*0.22)]" : ""}`} onClick={() => handleTabSelect(tableId)} style={{zIndex: zIndex}}>
+                <div className={"absolute w-full h-full flex flex-col items-center"}>
+                    <p className={"text-white"} style={{zIndex: zIndex+1}}>{fittedText}</p>
+                </div>
+                <TabSVG className={`${className}  stroke-fieldRing fill-fieldColor w-full h-full`}/>
+            </div>
+        )
+    }
+
+    const Tabs = () => {
+        return (
+            <div className={"flex w-full h-full"}>
+                {tables.map((table, index) => {
+                    if (index === 0) {
+                        return (
+                            <Tab includeOverlap={false} tableId={table.id} zIndex={8 - index} text={table.name}/>
+                        )
+                    } else {
+                        return (
+                            <Tab includeOverlap={true} tableId={table.id} zIndex={8 - index} text={table.name}/>
+                        )
+                    }
+                    })}
+            </div>
+        )
+    }
+
     return (
         <div className={"w-screen flex flex-col justify-start items-center"}>
+            <div className={"flex w-4/5"}>
+                <Tabs />
+            </div>
             <div className={"z-9 w-4/5"}>
                 <Table id={tableSelection} />
             </div>

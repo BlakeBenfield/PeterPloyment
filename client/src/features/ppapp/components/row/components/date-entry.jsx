aca9fb0e7c0b0ea3e9ff69548c757b07e04a316e
Blake Benfield <Blakefbenfield101@gmail.com>
2025-11-30 22:16:04 -0800

    Partial table restyling

--- client/src/features/ppapp/components/row/components/date-entry.jsx
+++ client/src/features/ppapp/components/row/components/date-entry.jsx
@@ -1,7 +1,7 @@
 const DateEntry = ({name, value, changeCB}) => {
     return (
         <>
-            <input name={name} className={"text-white focus:outline-none bg-fieldColor text-lg border-gray-400 border-1 w-[var(--cellWidth)] h-7"} type={"date"} value={value} maxLength={32} onChange={changeCB}/>
+            <input name={name} className={"text-white text-center focus:outline-none bg-fieldColor text-lg border-gray-400 border-1 w-[var(--cellWidth)] h-7"} type={"date"} value={value} maxLength={32} onChange={changeCB}/>
         </>
     );
 

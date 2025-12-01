c64cb17e47d0e805564672cbbe0a80778e12884c
Blake Benfield <Blakefbenfield101@gmail.com>
2025-11-30 21:20:51 -0800

    Implemented table tabs

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react'
-import '../global.css'
+import { useState } from 'react';
+import '../global.css';
+import { Routes, Route } from "react-router-dom";
 import Signup from "../features/signup/signup.jsx";
-
-const CounterButton = () => {
-    const [count, setCount] = useState(0);
-    return (
-        <button className="text-4xl font-bold w-screen text-center" onClick={() => {
-            setCount(count +1);
-        }}>
-            I've been clicked {count} times!
-        </button>
-    )
-}
+import Login from "../features/login/login.jsx";
+import Home from "../features/home/home.jsx";
+import Row from "../features/ppapp/components/row/row.jsx";
+import Note from "../features/ppapp/components/row/components/note.jsx";
+import Table from "../features/ppapp/components/table.jsx";
+import TextEntry from "../features/ppapp/components/row/components/text-entry.jsx";
+import DateEntry from "../features/ppapp/components/row/components/date-entry.jsx";
+import SelectEntry from "../features/ppapp/components/row/components/select-entry.jsx";
+import MultiTable from "../features/ppapp/components/multi-table.jsx";
 
 function App() {
 
-  return (
-      <>
-          <Signup />
-      </>
-  )
+    return (
+        <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/signup"} element={<Signup />} />
+            <Route path={"/login"} element={<Login />} />
+            {<Route path={"/app"} element={<MultiTable />} />}
+        </Routes>
+    )
 }
 
 export default App
--- client/src/features/ppapp/components/multi-table.jsx
+++ client/src/features/ppapp/components/multi-table.jsx
@@ -20,14 +20,22 @@ const MultiTable = () => {
         setTableSelection(id);
     }
 
+    //TODO Have text 'autofit' to tab. Some inspiration: https://sentry.engineering/blog/perfectly-fitting-text-to-container-in-react
     const Tab = ({className, tableId, includeOverlap, zIndex, text}) => {
-        let fittedText = text;
-        if (text.length > 8) fittedText = text.substring(0, 5) + '...';
-        if (tableSelection === tableId) zIndex += 50;
+        if (tableSelection === tableId) zIndex += 10;
+
+        const handleMouseEnter = (e) => {
+            if (e.currentTarget.scrollWidth > e.currentTarget.clientWidth) {
+                e.currentTarget.title = text;
+            } else {
+                e.removeAttribute("title");
+            }
+        }
+
         return (
-            <div className={`relative w-[var(--tabWidth)] cursor-pointer ${includeOverlap ? "-ml-[calc(var(--tabWidth)*0.22)]" : ""}`} onClick={() => handleTabSelect(tableId)} style={{zIndex: zIndex}}>
-                <div className={"absolute w-full h-full flex flex-col items-center"}>
-                    <p className={"text-white"} style={{zIndex: zIndex+1}}>{fittedText}</p>
+            <div className={`flex flex-col items-center relative w-[var(--tabWidth)] h-[var(--tabHeight)] cursor-pointer ${includeOverlap ? "-ml-[var(--tabOverlap)]" : ""}`} onClick={() => handleTabSelect(tableId)} style={{zIndex: zIndex}}>
+                <div className={"absolute w-3/5 h-full flex flex-col items-center justify-center"}>
+                    <p className={"text-white truncate w-full text-center"} style={{zIndex: zIndex+1}} onMouseEnter={handleMouseEnter}>{text}</p>
                 </div>
                 <TabSVG className={`${className}  stroke-fieldRing fill-fieldColor w-full h-full`}/>
             </div>
@@ -35,8 +43,15 @@ const MultiTable = () => {
     }
 
     const Tabs = () => {
+        const numTabs = Math.max(tables.length, 4);
+        const overlapRatio = 0.22;
         return (
-            <div className={"flex w-full h-full"}>
+            <div className={"flex w-full h-full"}
+            style={{
+                "--tabWidth": `min(calc((100% - 5*0.25rem)/(${numTabs} - ${overlapRatio}*${numTabs - 1})), 15rem)`,
+                "--tabHeight": "2rem",
+                "--tabOverlap": `calc(var(--tabWidth)*${overlapRatio}`
+            }}>
                 {tables.map((table, index) => {
                     if (index === 0) {
                         return (
@@ -57,7 +72,7 @@ const MultiTable = () => {
             <div className={"flex w-4/5"}>
                 <Tabs />
             </div>
-            <div className={"z-9 w-4/5"}>
+            <div className={"z-100 w-4/5 -mt-0.5"}>
                 <Table id={tableSelection} />
             </div>
         </div>

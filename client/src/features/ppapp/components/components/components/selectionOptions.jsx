d0a82709baab1e926efa417ca907aad0295b494a
Blake Benfield <Blakefbenfield101@gmail.com>
2025-12-15 20:39:32 -0800

    Updated option colors

--- client/src/features/ppapp/components/components/components/selectionOptions.jsx
+++ client/src/features/ppapp/components/components/components/selectionOptions.jsx
@@ -1,57 +1,57 @@
 export const statuses = [
     {
         text: "Unavailable",
-        color: "#8f8f8f"
+        color: "#808080"
     },
     {
         text: "Not applied",
-        color: "#e08d1f"
+        color: "#cb7f1e"
     },
     {
         text: "Applied",
-        color: "#ded218"
+        color: "#cbc81e"
     },
     {
         text: "Rejected",
-        color: "#b5090c"
+        color: "#cb1e1e"
     },
     {
         text: "Accepted",
-        color: "#398e16"
+        color: "#49cb1e"
     },
     {
         text: "In-progress",
-        color: "#6209b5"
+        color: "#7d1ecb"
     }
 ]
 
 export const preferences = [
     {
         text: "Perfect",
-        color: "#8f8f8f"
+        color: "#439d27"
     },
     {
         text: "Great",
-        color: "#e08d1f"
+        color: "#275a9d"
     },
     {
         text: "Good",
-        color: "#b5aa09"
+        color: "#279d7a"
     },
     {
         text: "Ok",
-        color: "#b5090c"
+        color: "#8d9d27"
     },
     {
         text: "Unfavorable",
-        color: "#3db509"
+        color: "#9d8127"
     },
     {
         text: "Bad",
-        color: "#6209b5"
+        color: "#9d5027"
     },
     {
         text: "Terrible",
-        color: "#ff0000"
+        color: "#9d2727"
     }
 ]
